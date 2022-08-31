c0641107187217ee087ae1df642ae53d27998964
beerbearli <beerbearli@foxmail.com>
2022-08-31 16:39:06 +0800

    that's all

--- MdTable.py
+++ MdTable.py
@@ -1,9 +1,12 @@
-# genMarkdownTable
-* en: data copy from sql transfer Markdown table
-* zh: 从mysql可视化工具复制的数据转化为标准的markdown表格
+import pyperclip as clip
 
-## main
-```python
+
+# markdown 标准表格格式
+# |  表头   | 表头  |
+# |  ----  | ----  |
+# | 单元格  | 单元格 |
+# | 单元格  | 单元格 |
+
 def gen_md_table(cp: str, sp="\r\n", sp_="\t", header=None):
     """
     生成markdown表格
@@ -11,7 +14,7 @@ def gen_md_table(cp: str, sp="\r\n", sp_="\t", header=None):
     :param sp: str 行末分隔符，默认"\r\n"
     :param sp_: str 同一行数据分隔符，默认"\t"
     :param header: list 表头列表，默认["字段名", "字段说明", "字段类型"]
-    :return: str
+    :return:
     """
     if header is None:
         header = ["字段名", "字段说明", "字段类型"]
@@ -24,4 +27,14 @@ def gen_md_table(cp: str, sp="\r\n", sp_="\t", header=None):
         str_return += '|' + '|'.join([_ for _ in s.split("\t")]) + '|\n'
     return str_return
 
-```
+
+def clip_it():
+    """
+    剪切板自动化读取写入
+    :return:
+    """
+    value = clip.paste()
+    clip.copy(gen_md_table(value))
+
+
+clip_it()
